3edf5c41d73a33fb18c9d8844493836ba38b7e45
mike <mpgarate@gmail.com>
2014-11-02 18:01:22 -0500

    ability to create a question

--- assignment3/question.sh
+++ assignment3/question.sh
@@ -22,15 +22,38 @@ initialize_directories(){
     fi
 }
 
+# store a question as a file
+# if a question already exists, exit with error
 create_question(){
     question_id="$1"
     question_text="$2"
     question_path="${BASE_DIR}questions/${question_id}"
 
-    if [ ! -f "$question_path" ]; then
+    if [ ! -f "$question_path" ]
+    then
         touch "$question_path"
         echo "$question_text" >> "$question_path"
+    else
+        echo "Error: Question already exists." >&2
+        exit 1
     fi
+}
+
+# returns 1 if invalid, 0 if valid
+is_valid_question(){
+    question_text="$1"
+
+    # if the question with all spaces removed is empty
+    if [ -z "${question_text//}"]; then
+        return 1
+    fi
+
+    # if the question contains ====
+    if [[ ${question_text} == *====* ]]; then
+        return 1
+    fi
+
+    return 0
 }
 
 ############################
@@ -39,6 +62,11 @@ create_question(){
 
 initialize_directories
 
+
 if [ "$1" == "create" ]; then
-    create_question "$2" "$3"
+    question_text=$3
+    if [ -z "$question_text" ]; then
+        read -p "Enter question: " question_text
+    fi
+    create_question "$2" "$question_text"
 fi
