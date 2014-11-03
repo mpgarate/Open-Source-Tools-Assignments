d7ca8e7d88c86dc3424174cf1b01986fb434c266
mike <mpgarate@gmail.com>
2014-11-02 19:31:25 -0500

    fixing question validation

--- assignment3/question.sh
+++ assignment3/question.sh
@@ -22,29 +22,11 @@ initialize_directories(){
     fi
 }
 
-# store a question as a file
-# if a question already exists, exit with error
-create_question(){
-    question_id="$1"
-    question_text="$2"
-    question_path="${BASE_DIR}questions/${question_id}"
-
-    if [ ! -f "$question_path" ]
-    then
-        touch "$question_path"
-        echo "$question_text" >> "$question_path"
-    else
-        echo "Error: Question already exists." >&2
-        exit 1
-    fi
-}
-
 # returns 1 if invalid, 0 if valid
 is_valid_question(){
-    question_text="$1"
 
     # if the question with all spaces removed is empty
-    if [ -z "${question_text//}"]; then
+    if [ -z "${question_text//}" ]; then
         return 1
     fi
 
@@ -56,17 +38,61 @@ is_valid_question(){
     return 0
 }
 
+# store a question as a file
+# if a question already exists, exit with error
+create_question(){
+    question_path="${BASE_DIR}questions/${question_id}"
+
+    if [ -f "$question_path" ]; then
+        echo "Error: Question already exists." >&2
+        exit 1
+    fi
+
+    if ! is_valid_question ; then
+        echo "Error: Invalid question." >&2
+        exit 1
+    fi
+
+    touch "$question_path"
+    echo "$question_text" >> "$question_path"
+}
+
+# create_answer(){
+# }
+
 ############################
 ###### Main Execution ######
 ############################
 
 initialize_directories
 
-
 if [ "$1" == "create" ]; then
+    question_id=$2
     question_text=$3
+
     if [ -z "$question_text" ]; then
         read -p "Enter question: " question_text
     fi
-    create_question "$2" "$question_text"
+
+    create_question
+    exit 0
+fi
+
+if [ "$1" == "answer" ]; then
+    question_id=$2
+    name=$3
+    answer=$4
+
+    if [ -z "answer" ]; then
+        read -p "Enter answer: " answer
+    fi
+
+    create_answer
+    exit 0
+fi
+
+## reset command for debugging 
+if [ "$1" == "reset" ]; then
+    rm -rf "$BASE_DIR"
+    exit 0
 fi
